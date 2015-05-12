962a26da83348552a8254542871ff359b2a9e162
Jull29 <agm_39_39@hotmail.com>
2015-05-12 14:54:59 -0500

    hoy

---
+++ AppNorthwindMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppNorthwindMVC.MvcApplication" Language="C#" %>
---
+++ AppNorthwindMVC/NORTHWNDEntities.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using AppNorthwindMVC.Models;
+
+namespace AppNorthwindMVC
+{
+    public class NORTHWNDEntities : DbContext
+    {
+        // Puede agregar código personalizado a este archivo. Los cambios no se sobrescribirán.
+        // 
+        // Si desea que Entity Framework lo omita y regenere la base de datos
+        // automáticamente siempre que cambie el esquema de modelo, agregue
+        // el siguiente código al método Application_Start en el archivo Global.asax.
+        // Nota: esta operación destruirá y volverá a crear la base de datos con cada cambio de modelo.
+        // 
+        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<AppNorthwindMVC.NORTHWNDEntities>());
+
+        public NORTHWNDEntities() : base("name=NORTHWNDEntities")
+        {
+        }
+
+        public DbSet<Employees> Employees { get; set; }
+    }
+}
