18515c9e375ce8a676bba64b3e3b73ba96faace5
Jull29 <agm_39_39@hotmail.com>
2015-05-12 22:24:38 -0500

    Ultimo cambio. sk8

---
+++ AppNorthwindMVC/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppNorthwindMVC.Models;
+
+namespace AppNorthwindMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private NORTHWNDEntities db = new NORTHWNDEntities();
+
+        //
+        // GET: /Products/
+
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Categories).Include(p => p.Suppliers);
+            return View(products.ToList());
+        }
+
+        //
+        // GET: /Products/Details/5
+        public ActionResult Search(string text)
+        {
+            var results = db.Products.Find(text);
+            return PartialView("ResultsPartial", results);
+        }
+
+       
+
+       
+    }
+}
--- AppNorthwindMVC/NORTHWNDEntities.cs
+++ AppNorthwindMVC/NORTHWNDEntities.cs
@@ -19,5 +19,11 @@ namespace AppNorthwindMVC
         }
 
         public DbSet<Employees> Employees { get; set; }
+
+        public DbSet<Products> Products { get; set; }
+
+        public DbSet<Categories> Categories { get; set; }
+
+        public DbSet<Suppliers> Suppliers { get; set; }
     }
 }
